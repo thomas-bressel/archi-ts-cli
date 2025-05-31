752a246ce00247e42bca373124bae8bd1eee5fef
thomas-bressel <tbressel@outlook.fr>
2025-05-31 17:31:47 +0200

    [1.3.0] Archi TS CLI

--- cmd/root.go
+++ cmd/root.go
@@ -9,7 +9,7 @@ var rootCmd = &cobra.Command{
 	Short: "Archi TS CLI - Backend Architecture Generator by Thomas Bressel",
 	Long: `Archi TS is a CLI tool for generating backend architectures and components.
 It helps you quickly scaffold projects with clean architecture patterns.`,
-	Version: "1.1.0",
+	Version: "1.3.0",
 }
 
 /*
